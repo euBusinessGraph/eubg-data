43efeac971fa1227e6d7e6b8b68ca1443c4943bd
Vladimir Alexiev <vladimir.alexiev@ontotext.com>
2018-10-03 16:14:05 +0300

    minor fixes to tarql, generate ttl files

---
+++ data/lookups/EBG-lookups.sh
@@ -0,0 +1,2 @@
+tarql --encoding utf-8 --base http://data.businessgraph.io/ --write-base EBG-company-status.tarql EBG-company-status.csv > EBG-company-status.ttl
+tarql --encoding utf-8 --base http://data.businessgraph.io/ --write-base EBG-company-type.tarql   EBG-company-type.csv   > EBG-company-type.ttl
