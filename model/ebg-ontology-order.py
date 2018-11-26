23c3d5f3841e4d65439f462b394285a72474162f
Javier Paniagua <paniagua@spaziodati.eu>
2018-11-26 15:05:26 +0100

    identifiersystem examples, officers and memberships

--- model/ebg-ontology-order.py
+++ model/ebg-ontology-order.py
@@ -61,11 +61,14 @@ if args.input is not None:
 else:
     fd = sys.stdin
 
+order_triples = []
+last_not_empty = None
 with fd:
     for line in fd:
         order = None
         clean_line = line.strip()
         tokens = [t for t in line.strip().split(' ') if t != '']
+        last_not_empty = len(clean_line) > 0
         if len(tokens) > 1 and tokens[1] == 'a':
             if (len(tokens) == 3 and clean_line.endswith(';')):
                 property_type = tokens[2][:-1]
@@ -96,6 +99,16 @@ with fd:
                 token = None
             if token and is_natural(token):
                 continue
+        elif (len(tokens) == 3 and clean_line.endswith('.')) or (len(tokens) == 4 and tokens[-1] == '.'):
+            if tokens[1] == 'ebg:order':
+                continue
         print(line.rstrip())
         if order is not None:
-            print('  ebg:order {} ;'.format(order))
+            order_triples.append((property, order))
+            # print('  ebg:order {} ;'.format(order))
+
+if order_triples:
+    if last_not_empty:
+        print('')
+    for triple in sorted(order_triples, key=lambda x: x[1]):
+        print('{} ebg:order {}.'.format(triple[0], triple[1]))
